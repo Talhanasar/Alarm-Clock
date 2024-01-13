36f5a024d512a906d148655444caba4d0a1bd342
Talhanasar <talhabinnasar7@gmail.com>
2024-01-13 22:24:59 +0600

    updated all

--- script.js
+++ script.js
@@ -1,67 +1,92 @@
-let btn = document.querySelector('.btn');
-let user_time;
-let hour_box = document.querySelector('.hour');
-let min_box = document.querySelector('.min');
-let sec_box = document.querySelector('.sec');
+let alarm_btn = document.querySelector('#check');
+let full_time = document.querySelector('.full-time');
 let ampm = document.querySelector('.ampm');
-let time_box = document.querySelector('.time');
-let timer = document.querySelector('.form');
+let timer = document.querySelector('.blur');
 let err = document.getElementById('err');
 let alarm = new Audio("alarm.mp3");
 let hr_remain = document.querySelector('.hr-remain');
 let min_remain = document.querySelector('.min-remain');
+let set_btn = document.getElementById('btn');
+let selectData = document.getElementsByTagName('select');
+let hr, min, time, hr_, min_, ampm_;
+let on = false;
 
-btn.addEventListener('click',()=>{
-    user_time = document.getElementById('time').value;
-    let userhour = user_time.slice(0,2);
-    let usermin = user_time.slice(3,5);
-    if(user_time == ''){
-        err.style.display = 'block'
+alarm_btn.addEventListener('change', () => {
+    if (alarm_btn.checked) {
+        timer.style.display = 'block';
     }else{
-        timer.style.display = 'none';
-        time(userhour, usermin);
-        time_box.style.display = 'block';
+        alarm.pause();
+        alarm.currentTime = 0;
+        on = false;
     }
-});
+})
+set_btn.addEventListener('click', () => {
+    hr = selectData[0].value;
+    min = selectData[1].value;
+    let ampm = selectData[2].value;
+    if (hr >= hr_ && min >= min_ && ampm == ampm_) {
+        timer.style.display = 'none';
+        on = true;
+    } else { err.style.display = 'block'; }
+})
 
-function time_remain(user_H,user_M,H,M){
-    hr_remain.innerHTML = (user_H-H);
-    min_remain.innerHTML = (user_M - M);
+for (let index = 12; index >= 1; index--) {
+    if (index < 10) {
+        index = `0${index}`;
+    } else {
+        index = index
+    }
+    let option = `<option value = "${index}">${index}</option>`;
+    selectData[0].firstElementChild.insertAdjacentHTML("afterend", option)
 }
 
-function time(user_H, user_M){
-    setInterval(() => {
-        let a = new Date();
-        let hour = a.getHours();
-        let min = a.getMinutes();
-        let sec = a.getSeconds();
-        let time ;
-        let hour_12 = hour;
-        if(hour>12){
-            hour_12 = hour - 12;
-            time = 'PM';
-        }else if (hour == 12 && min > 0){
-            time = 'PM';
-        }else{
-            time = 'AM';
-        }
-        hour_box.innerHTML = hour_12;
-        if(min<10){
-            min_box.innerHTML = `0${min}`;
-        }else{
-            min_box.innerHTML = min;
-        }if(sec<10){
-            sec_box.innerHTML = `0${sec}`;
-        }else{
-            sec_box.innerHTML = sec;
-        }
-        ampm.innerHTML = time;
-
-        time_remain(user_H,user_M,hour,min);
-        if(user_H == hour && user_M == min){
-            alarm.play();
-        }
-
-    }, 1000);
+for (let index = 59; index >= 1; index--) {
+    if (index < 10) {
+        index = `0${index}`;
+    } else {
+        index = index
+    }
+    let option = `<option value = "${index}">${index}</option>`;
+    selectData[1].firstElementChild.insertAdjacentHTML("afterend", option)
 }
+
+for (let index = 0; index < 2; index++) {
+    let ampm;
+    if (index == 1) {
+        ampm = "AM"
+    } else {
+        ampm = "PM"
+    }
+    let option = `<option value = "${ampm}">${ampm}</option>`;
+    selectData[2].firstElementChild.insertAdjacentHTML("afterend", option)
+}
+
+function time_remain(user_H, user_M, H, M) {
+    console.log(user_H, user_M, H, M);
+    if (user_H != null || user_M != null) {
+        hr_remain.innerHTML = (user_H - H);
+        min_remain.innerHTML = (user_M - M);
+    }
+}
+setInterval(() => {
+    let a = new Date();
+    time = a.toLocaleTimeString();
+    hr_ = a.getHours();
+    min_ = a.getMinutes();
+    if (hr_ > 12) {
+        hr_ = hr_ - 12
+    }
+    if (hr_ < 10) {
+        hr_ = `0${hr_}`;
+    }
+
+    full_time.innerHTML = time.slice(0, time.length - 2);
+    ampm.innerHTML = time.slice(time.length - 2, time.length);
+    ampm_ = time.slice(time.length - 2, time.length);
+    time_remain(hr, min, hr_, min_);
+    if (hr==hr_ && min == min_ && on==true) {
+        alarm.play();
+    }
+
+}, 1000);
 
